4e5047c30116e93d52ae26dd16856a63528c11ee
SemTeuwsen <semteuwsen@live.nl>
2015-03-23 15:16:36 +0100

    ok

--- app/src/main/java/fallingbird/ObstacleSpawner.java
+++ app/src/main/java/fallingbird/ObstacleSpawner.java
@@ -5,25 +5,19 @@ import android.gameengine.icadroids.alarms.IAlarm;
 import android.gameengine.icadroids.engine.GameEngine;
 import android.util.Log;
 
-import com.android.vissenspel.Strawberry;
-import com.android.vissenspel.Vissenkom;
-
-/**
- * Created by Luuk on 18-3-2015.
- */
-public class ObstacleSpawner implements IAlarm{
+class ObstacleSpawner implements IAlarm{
 
     private FallingBird mygame;
     private Alarm BerryAlarm;
+    private Alarm BranchAlarm;
 
     private int ObstacleSpeed;
-    private Alarm BranchAlarm;
 
     public ObstacleSpawner(FallingBird FB){
         mygame = FB;
 
         BerryAlarm = new Alarm(1, 1, this);
-        BranchAlarm = new Alarm(2,2,this);
+        BranchAlarm = new Alarm(2,1 ,this);
 
         BerryAlarm.startAlarm();
         BranchAlarm.startAlarm();
@@ -33,27 +27,43 @@ public class ObstacleSpawner implements IAlarm{
 
     public void triggerAlarm(int id) {
         if (id == 1) {
-           // Log.d("alarm test", "geslaagd!" + id);
-            BerryAlarm.setTime(10 + (int) (1 * Math.random()));
+            BerryAlarm.setTime(10 + (int) (100 * Math.random()));
             BerryAlarm.restartAlarm();
             Berry s = new Berry(mygame, ObstacleSpeed);
-            // world size has not been fixed, put it in a block of 600*400 pixels
             int y = 10 + (int) (990 * Math.random());
 
             mygame.addGameObject(s, GameEngine.getScreenWidth(), y);
         }
 
         else if (id == 2) {
+            String side;
+            side = setRandomSide();
             BranchAlarm.setTime(10 + (int) (80 * Math.random()));
             BranchAlarm.restartAlarm();
-            Branch r = new Branch(mygame, "right", ObstacleSpeed);
-            Branch l = new Branch(mygame, "left", ObstacleSpeed);
-            Log.d("alarm test", "Branch alarm!" + id);
+            Branch r = new Branch(mygame, side, ObstacleSpeed);
             mygame.addGameObject(r, GameEngine.getScreenWidth(), 0);
-            mygame.addGameObject(l, GameEngine.getScreenWidth(), GameEngine.getScreenHeight()-300);
+            if(side == "right"){
+                mygame.addGameObject(r, GameEngine.getScreenWidth(), 0);
+            }
+            else if(side == "left") {
+                mygame.addGameObject(r, GameEngine.getScreenWidth(), GameEngine.getScreenHeight()-300);
+            }
+            else {
+                mygame.addGameObject(r, GameEngine.getScreenWidth(), GameEngine.getScreenHeight()/2-150);
+            }
         }
-        else if (id==3) {
-            
+
+    }
+    private String setRandomSide(){
+        double side = (Math.random());
+        if(side < 0.3333){
+            return "right";
+        }
+        else if(side>0.3333 && side<0.6666){
+            return "left";
+        }
+        else {
+            return "center";
         }
     }
 }
